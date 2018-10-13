2fa206fc0d9acdcb4400c4b3ab70b5ba49f967d8
Gautam <g417ks@gmail.com>
2018-10-13 06:17:47 +0530

    last-step-required

--- app/src/main/java/com/example/gautam/chattingapp/ChattingActivity.java
+++ app/src/main/java/com/example/gautam/chattingapp/ChattingActivity.java
@@ -1,5 +1,6 @@
 package com.example.gautam.chattingapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +9,31 @@ import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 public class ChattingActivity extends AppCompatActivity {
-
+    public static String user_name="abc";
+    public static String user_phone_no="def";
+    public static String user_Id="ghi";
+    public static String friend_name="jkl";
+    public static String friend_phone_no="mno";
+    public static String friend_Id="pqr";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
         Toolbar toolbar=(Toolbar)findViewById(R.id.chatting_toolbar);
         setSupportActionBar(toolbar);
-        setTitle("chatting person");
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        Intent intent=getIntent();
+        String chatname=intent.getStringExtra(user_name);
+        String chatnumber=intent.getStringExtra(user_phone_no);
+        String chatid=intent.getStringExtra(user_Id);
+        String friendname=intent.getStringExtra(friend_name);
+        String friendnumber=intent.getStringExtra(friend_phone_no);
+        String friendid=intent.getStringExtra(friend_Id);
+        setTitle(friendname);
+
+
+
     }
 
 
--- app/src/main/java/com/example/gautam/chattingapp/MainActivity.java
+++ app/src/main/java/com/example/gautam/chattingapp/MainActivity.java
@@ -15,12 +15,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -48,11 +54,15 @@ public class MainActivity extends AppCompatActivity {
     private   ListView listView;
     private ArrayAdapter<String> arrayAdapter;
     final private int PERMISSION_REQUEST_CODE = 124;
-    private ArrayList<String>users=new ArrayList<>();
-   private ArrayList<String> contactlist=new ArrayList<>();
-   private ArrayList<String> contact_name=new ArrayList<>();
-   private ArrayList<String> friendList=new ArrayList<>();
-   private ArrayList<String> friend_contacts=new ArrayList<>();
+   private String currentUserName;
+   private String currentuserphoneno;
+   private ArrayList<String> contactlist=new ArrayList<>(); //total mobile contacts
+   private ArrayList<String> appUsers_name=new ArrayList<>();// Android Chat users   appUsers_name
+   private ArrayList<String> appUsers_no=new ArrayList<>();//contact no of Android Chat users
+    private ArrayList<String>appUsers_Id=new ArrayList<>();
+    private ArrayList<String> user_friendlist=new ArrayList<>();//current users friendlist
+    private ArrayList<String> user_friendlist_no=new ArrayList<>();//contact no current users friendlist
+    private ArrayList<String> user_friends_Id=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         fbAuth=FirebaseAuth.getInstance();
         setTitle("Friend List");
-         listView=(ListView) findViewById(R.id.chatlist);
+
          currentUserUid = fbAuth.getCurrentUser().getUid();
         database=FirebaseDatabase.getInstance();
         dbRef=database.getReference();
@@ -75,26 +85,42 @@ public class MainActivity extends AppCompatActivity {
                 dbRef.child("users").child(currentUserUid).child("name").setValue(intent.getStringExtra(u_name));
                 ss=intent.getStringExtra(p_no);
                dbRef.child("users").child(currentUserUid).child("phone").setValue(ss);
-                dbRef.child("userlist").child(ss).setValue(intent.getStringExtra(u_name));
+                dbRef.child("userlist").child(ss).child("name").setValue(intent.getStringExtra(u_name));
+                dbRef.child("userlist").child(ss).child("userId").setValue(currentUserUid);
             }
         }
-         arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,contactlist);
+        user_friendlist.clear();
+        listView=(ListView) findViewById(R.id.chatlist);
+         arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,user_friendlist);
         listView.setAdapter(arrayAdapter);
        // getCallingActivity().getClassName();
-        dbRef2=database.getReference("/users/"+currentUserUid+"/friends");
-         query	=	dbRef2.orderByKey();
+        dbRef2=database.getReference("/users/"+currentUserUid);
+
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Intent intent1=new Intent(MainActivity.this,ChattingActivity.class);
+                intent1.putExtra(ChattingActivity.user_name,currentUserName);
+               intent1.putExtra(ChattingActivity.user_phone_no,currentuserphoneno);
+               intent1.putExtra(ChattingActivity.user_Id,currentUserUid);
+                intent1.putExtra(ChattingActivity.friend_name,user_friendlist.get(position));
+               intent1.putExtra(ChattingActivity.friend_phone_no,user_friendlist_no.get(position));
+               intent1.putExtra(ChattingActivity.friend_Id,user_friends_Id.get(position));
+                startActivity(intent1);
+           }
+       });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        query.addListenerForSingleValueEvent(queryValueListener);
+           dbRef2.addListenerForSingleValueEvent(queryValueListener);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        dbRef2.removeEventListener(queryValueListener);
+       dbRef2.removeEventListener(queryValueListener);
        // dbRef1.removeEventListener(childEventListener);
     }
 
@@ -108,46 +134,112 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId())
         {
-            case R.id.refresh1 :{  AccessContact(); break;
-            }
-            case R.id.sign_out :{
-                fbAuth.signOut();
-                Intent intent=new Intent(MainActivity.this,Authentication.class);
-                startActivity(intent);
+            case R.id.profile :  {
 
-            }
+
+                                   } break;
+
+            case R.id.refresh1 :{
+                                    appUsers_no.clear();
+                                    appUsers_name.clear();
+                                    appUsers_Id.clear();
+                                    dbRef=database.getReference("/userlist");
+                                     dbRef.addListenerForSingleValueEvent(new ValueEventListener()
+                                     {
+                                  @Override
+                                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                  Iterable<DataSnapshot>	snapshotIterator	=	dataSnapshot.getChildren();
+                                 Iterator<DataSnapshot> iterator	=	snapshotIterator.iterator();
+
+                                 while	(iterator.hasNext())	{           //we need to get the current no of user from here using DataSnapshot;
+                                  DataSnapshot	next	=	(DataSnapshot)	iterator.next();
+                                 appUsers_no.add(next.getKey());
+                                 appUsers_name.add(next.child("name").getValue(String.class));
+                                 appUsers_Id.add(next.child("userId").getValue(String.class));
+                                       }
+                                  AccessContact();
+                                 }
+
+                                 @Override
+                                 public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                 });
+                                }
+                                break;
+
+            case R.id.sign_out :    {
+                                         fbAuth.signOut();
+                                         Intent intent=new Intent(MainActivity.this,Authentication.class);
+                                         startActivity(intent);
+
+                                     }
         }
         return super.onOptionsItemSelected(item);
     }
     private ValueEventListener  queryValueListener= new ValueEventListener() {
 
         @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            if(!dataSnapshot.exists())
-            {
-                AccessContact();
-            }
-            else
-            {
-                contactlist.clear();
-                contact_name.clear();
-                Iterable<DataSnapshot>	snapshotIterator	=	dataSnapshot.getChildren();
-                Iterator<DataSnapshot> iterator	=	snapshotIterator.iterator();
 
-                while	(iterator.hasNext())	{
+            public void onDataChange (@NonNull DataSnapshot dataSnapshot) {
+                currentuserphoneno=dataSnapshot.child("phone").getValue(String.class);
+                currentUserName=dataSnapshot.child("name").getValue(String.class);
+                if (dataSnapshot.child("friends").exists()) {
+                    user_friendlist.clear();
+                    user_friendlist_no.clear();
+                    user_friends_Id.clear();
+                    Iterable<DataSnapshot> snapshotIterator = dataSnapshot.child("friends").getChildren();
+                    Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
 
-                    DataSnapshot	next	=	(DataSnapshot)	iterator.next();
-                    contactlist.add(next.getValue().toString());
+                    while (iterator.hasNext()) {           //we need to get the current no of user from here using DataSnapshot;
+                        DataSnapshot next = (DataSnapshot) iterator.next();
+                        user_friendlist.add(next.child("name").getValue(String.class));
+                        user_friendlist_no.add(next.child("phone").getValue(String.class));
+                        user_friends_Id.add(next.child("userId").getValue(String.class));
+                    }
+                    arrayAdapter.notifyDataSetChanged();
+
+
+                } else {
+
+                    appUsers_name.clear();
+                    appUsers_no.clear();
+                    appUsers_Id.clear();
+                    dbRef=dbRef.child("userlist");
+                    dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+
+                            for (DataSnapshot next : snapshotIterator) {
+
+                                appUsers_no.add(next.getKey());
+                                appUsers_name.add(next.child("name").getValue(String.class));
+                                appUsers_Id.add(next.child("userId").getValue(String.class));
+
+
+                            }
+                          //  String sss="abcd="+appUsers_no.size();
+                            //Toast.makeText(getApplicationContext(),appUsers_no.get(0), Toast.LENGTH_LONG).show();
+                            AccessContact();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(getApplicationContext(), "cancelled_1", Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
                 }
-                arrayAdapter.notifyDataSetChanged();
-            }
-        }
-
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError) {
 
         }
+
+            @Override
+            public void onCancelled (@NonNull DatabaseError databaseError){
+           Toast.makeText(getApplicationContext(), "cancelled_2", Toast.LENGTH_SHORT).show();
+        }
+
     };
 
     private void AccessContact()
@@ -215,43 +307,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void displayContacts() {
+    private void displayContacts()
+    {
         contactlist.clear();
+       user_friendlist.clear();
+       user_friendlist_no.clear();
+       user_friends_Id.clear();
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
-            database.getReference("/users/"+currentUserUid+"/friends").removeValue();
-        if (cur.getCount() > 0) {
-            while (cur.moveToNext()) {
+
+             database.getReference("/users/"+currentUserUid+"/friends").removeValue();
+          if (cur.getCount() > 0)
+          {
+            while (cur.moveToNext())
+            {
                 String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                if (Integer.parseInt(cur.getString(
-                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                    Cursor pCur = cr.query(
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
+                {
+                      Cursor pCur = cr.query(
                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                             null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
                             new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        if(phoneNo.length()==13)
+                            while (pCur.moveToNext())
+                         {
+                           String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                           if(phoneNo.length()==13)
                             contactlist.add(phoneNo.substring(3));
-                        if(phoneNo.length()==11)
+                            if(phoneNo.length()==11)
                             contactlist.add(phoneNo.substring(1));
-                        if(phoneNo.length()==10)
+                            if(phoneNo.length()==10)
                             contactlist.add(phoneNo);
-                        if(phoneNo.length()>=10) {
-                            contact_name.add(name);
-                            DatabaseReference newChildRef = dbRef2.push();
-                            String key = newChildRef.getKey();
-                            dbRef2.child(key).child("name").setValue(name);
-                            dbRef2.child(key).child("contact_no").setValue(phoneNo);
-                            arrayAdapter.notifyDataSetChanged();
-                        }
-                        }
-                    pCur.close();
+                         }
+                       pCur.close();
                 }
+             }
+             }
+
+            for(int i=0;i<appUsers_no.size();i++)
+           {  if(!(appUsers_no.get(i).equals(currentuserphoneno)))
+            for(int j=0;j<contactlist.size();j++) //for finding the no of friend using chatting app in contact list;
+            {
+               if((contactlist.get(j)).equals(appUsers_no.get(i)))
+               {
+                   user_friendlist.add(appUsers_name.get(i));
+                   user_friendlist_no.add(appUsers_no.get(i));
+                   user_friends_Id.add(appUsers_Id.get(i));
+                    break;
+               }
+
             }
-        }
+            arrayAdapter.notifyDataSetChanged();
+           }
+
+        DatabaseReference newChildReff =database.getReference().child("users").child(currentUserUid).child("friends");
+
+        for(int i=0;i<user_friendlist_no.size();i++)
+          {   DatabaseReference dsss;
+            dsss = newChildReff.push();
+            dsss.child("name").setValue(user_friendlist.get(i));
+            dsss.child("phone").setValue(user_friendlist_no.get(i));
+            dsss.child("userId").setValue(user_friends_Id.get(i));
+           }
+       cur.close();
+        dbRef=database.getReference();
     }
 }
