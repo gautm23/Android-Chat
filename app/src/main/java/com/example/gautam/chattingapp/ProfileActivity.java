c1228f93e9567b9fd851d1a486ca298579b91550
Gautam <g417ks@gmail.com>
2019-01-25 08:27:03 +0530

    Finish

--- app/src/main/java/com/example/gautam/chattingapp/ProfileActivity.java
+++ app/src/main/java/com/example/gautam/chattingapp/ProfileActivity.java
@@ -4,24 +4,42 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.ParcelFileDescriptor;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ProfileActivity extends AppCompatActivity {
     public static String Nm="rf";
     public static String ph="dc";
     public static String uuid="cr";
-    ImageView imageView;
+    public static String PROFILE_PHOTO="USER";
+    private Bitmap bmp ;
+    private Bitmap bitmap;
+    private ImageView imageView;
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +48,22 @@ public class ProfileActivity extends AppCompatActivity {
         TextView textView=(TextView)findViewById(R.id.textView);
         TextView textView2=(TextView)findViewById(R.id.textView2);
         TextView textView3=(TextView)findViewById(R.id.textView3);
-        imageView =(ImageView) findViewById(R.id.imageView);
-        textView.setText("Name:"+intent.getStringExtra(Nm));
-        textView2.setText("contact:"+intent.getStringExtra(ph));
+        imageView =(ImageView) findViewById(R.id.image);
+        preferences = getSharedPreferences("abcde", Context.MODE_PRIVATE);
+        String user_pic=preferences.getString(PROFILE_PHOTO, "__");
+        if(user_pic.equals("__"))
+        {
+        imageView.setImageResource(R.drawable.common_google_signin_btn_icon_dark_normal);
+            SharedPreferences.Editor editor=preferences.edit();
+            editor.putString(PROFILE_PHOTO,"__");
+            editor.apply();
+        }
+        else
+        {   bitmap = BitmapFactory.decodeFile(user_pic);
+            imageView.setImageBitmap(bitmap);
+        }
+        textView.setText(intent.getStringExtra(Nm));
+        textView2.setText("contact: "+intent.getStringExtra(ph));
         textView3.setText("uniqueId:"+intent.getStringExtra(uuid));
 
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -41,38 +72,59 @@ public class ProfileActivity extends AppCompatActivity {
             AccessGallery();
             }});
 
-    }
+   }
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) {
+        if (resultCode == RESULT_OK) {
 
             if (requestCode == 1 && data != null) {
-                Uri imageUri = data.getData();
-                imageView = (ImageView) findViewById(R.id.imageView);
-                imageView.setImageURI(imageUri);
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+               // String picturePath = cursor.getString(columnIndex);
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.putString(PROFILE_PHOTO,cursor.getString(columnIndex));
+                editor.apply();
+                Bitmap realImage = BitmapFactory.decodeFile(cursor.getString(columnIndex));
+                cursor.close();
+                imageView.setImageBitmap(realImage);
+               /* try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    imageView.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }*/
+
             }
-        }
+            else {
+                Toast.makeText(this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+            }
+            }
+
+
     }
     private void AccessGallery()
     {
 
-        String message = "You need to grant access to Manifest.permission.READ_CONTACTS";
-        if (!hasRuntimePermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE))
+        String message = "You need to grant access to Manifest.permission.READ_EXTERNAL_STORAGE";
+        if (!hasRuntimePermission(getApplicationContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)||!hasRuntimePermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
         {
-            requestRuntimePermission(ProfileActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE, 1111);
+            requestRuntimePermission(ProfileActivity.this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1111);
         } else
         {
             Intent intent = new Intent(Intent.ACTION_PICK,
                     MediaStore.Images.Media.INTERNAL_CONTENT_URI);
             intent.setType("image/*");
-            intent.putExtra("crop", "true");
-            intent.putExtra("scale", true);
-            intent.putExtra("outputX", 256);
-            intent.putExtra("outputY", 256);
-            intent.putExtra("aspectX", 85);
-            intent.putExtra("aspectY", 99);
-            intent.putExtra("return-data", true);
             startActivityForResult(intent, 1);
         }
     }
@@ -94,9 +146,9 @@ public class ProfileActivity extends AppCompatActivity {
         }
         return ret;
     }
-    private void requestRuntimePermission(Activity activity, String runtimePermission, int requestCode)
+    private void requestRuntimePermission(Activity activity, String [] runtimePermission, int requestCode)
     {
-        ActivityCompat.requestPermissions(activity, new String[]{runtimePermission}, requestCode);
+        ActivityCompat.requestPermissions(activity, runtimePermission, requestCode);
     }
 
     public void onRequestPermissionsResult(int requestCode,    String[] permissions, int[] grantResults)
@@ -114,13 +166,6 @@ public class ProfileActivity extends AppCompatActivity {
                     Intent intent = new Intent(Intent.ACTION_PICK,
                             MediaStore.Images.Media.INTERNAL_CONTENT_URI);
                     intent.setType("image/*");
-                    intent.putExtra("crop", "true");
-                    intent.putExtra("scale", true);
-                    intent.putExtra("outputX", 256);
-                    intent.putExtra("outputY", 256);
-                    intent.putExtra("aspectX", 85);
-                    intent.putExtra("aspectY", 99);
-                    intent.putExtra("return-data", true);
                     startActivityForResult(intent, 1);
                 }
                 else
@@ -128,6 +173,8 @@ public class ProfileActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"You denied Manifest.permission.READ_EXTERNAL_STORAGE permissions", Toast.LENGTH_LONG).show();
                 }
             }
+
         }
     }
+
 }
--- app/src/main/java/com/example/gautam/chattingapp/SignIn_Signup.java
+++ app/src/main/java/com/example/gautam/chattingapp/SignIn_Signup.java
@@ -1,6 +1,7 @@
 package com.example.gautam.chattingapp;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,11 +16,21 @@ import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 public class SignIn_Signup extends AppCompatActivity {
@@ -32,24 +43,27 @@ public class SignIn_Signup extends AppCompatActivity {
     private Button verifyButton;
     private String phoneNumber;
     private String phoneVerificationId;
+    private FirebaseDatabase database;
+    private ArrayList<String>active_users_list=new ArrayList<>();
+    private DatabaseReference dbRef;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks
             verificationCallbacks;
     private PhoneAuthProvider.ForceResendingToken resendToken;
-
+    private  Intent intent;
     private FirebaseAuth fbAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in__signup);
-       /* Toolbar toolbar=(Toolbar) findViewById(R.id.signup_toolbar);
-        setSupportActionBar(toolbar);*/
          username=(EditText) findViewById(R.id.editText);
         phonenumber = (EditText) findViewById(R.id.editText2);
         code = (EditText) findViewById(R.id.editText3);
         verifyButton = (Button) findViewById(R.id.button2);
         sendButton = (Button) findViewById(R.id.button);
-        Intent intent=getIntent();
+        database=FirebaseDatabase.getInstance();
+        dbRef=database.getReference("/userlist");
+       intent=getIntent();
 
         if(intent.getStringExtra(signinup).equals("sign_in")) {
             username.setVisibility(View.GONE);
@@ -62,14 +76,10 @@ public class SignIn_Signup extends AppCompatActivity {
 
     public void sendme(View view) {
         phoneNumber = phonenumber.getText().toString();
-        setUpVerificatonCallbacks();
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber,        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                SignIn_Signup.this,               // Activity (for callback binding)
-                verificationCallbacks);
-        //verifyButton.setEnabled(true);
+        dbRef.addListenerForSingleValueEvent(queryChildEventListener);
+       // Toast.makeText(getApplicationContext(),"abccccc", Toast
+         //       .LENGTH_SHORT).show();
+
     }
 
     private void setUpVerificatonCallbacks() {
@@ -141,5 +151,87 @@ public class SignIn_Signup extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+   private ValueEventListener queryChildEventListener=new ValueEventListener() {
+       @Override
+       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+           Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+           Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+             active_users_list.clear();
+           while (iterator.hasNext()) {           //we need to get the current no of user from here using DataSnapshot;
+               DataSnapshot next = (DataSnapshot) iterator.next();
+               active_users_list.add(next.getKey());
+           }
+           if(intent.getStringExtra(signinup).equals("sign_in")) {
+               int abc=0;
+               for(int i=0;i<active_users_list.size();i++)
+               {
+                   if((active_users_list.get(i)).equals(phoneNumber.substring(3)))
+                   {
+                       abc=1;
+                       break;
+
+
+                   }
+               }
+               if(abc==0)
+               {
+                   Toast.makeText(getApplicationContext(), "User doesn't exists please\n try signup !!", Toast
+                           .LENGTH_SHORT).show();
+               }
+               else{
+                   // Toast.makeText(getApplicationContext(), "cancelled_1", Toast.LENGTH_SHORT).show();
+                   setUpVerificatonCallbacks();
+                   PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                           phoneNumber,        // Phone number to verify
+                           60,                 // Timeout duration
+                           TimeUnit.SECONDS,   // Unit of timeout
+                           SignIn_Signup.this,               // Activity (for callback binding)
+                           verificationCallbacks);
+               }
+           }
+           else {
+               int abc=0;
+               for (int i = 0; i < active_users_list.size(); i++)
+               {
+                   if((active_users_list.get(i)).equals(phoneNumber.substring(3)))
+                   {
+                       Toast.makeText(getApplicationContext(), "User already exists please \ntry signin !!", Toast
+                               .LENGTH_SHORT).show();
+                       abc=1;
+                       break;
+
+                   }
+               }
+               if(abc==0)
+               {
+                   setUpVerificatonCallbacks();
+                   PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                           phoneNumber,        // Phone number to verify
+                           60,                 // Timeout duration
+                           TimeUnit.SECONDS,   // Unit of timeout
+                           SignIn_Signup.this,               // Activity (for callback binding)
+                           verificationCallbacks);
+                   //verifyButton.setEnabled(true);
+               }
+           }
+
+       }
+
+       @Override
+       public void onCancelled(@NonNull DatabaseError databaseError) {
+
+       }
+   };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
